50074749b7f2e6322f25ec9e94fc87903ae326bd
Luis Fernando Torres Siles <ltorres@impesa.net>
2022-12-16 16:53:28 -0600

    CI  & CD configuration

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,14 @@ const secretKey = `Impesa123456&`;
 const password = `fd505804-3302-439b-8780-a4ab8f6083f8`;
 const password = `fd505804-3302-439b-8780-a4ab8f6083f8`;
 
+var connection = {
+  host: 'localhost',
+  user: 'admin',
+  database: 'project',
+  password: 'mypassword', // sensitive
+  multipleStatements: true,
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
