44bd22c6ecf300bd23a2dbb28c3c47f4b06c6bb8
Luis Fernando Torres Siles <ltorres@impesa.net>
2022-12-16 16:49:35 -0600

    CI  & CD configuration part XIII

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 const secretKey = `Impesa123456&`;
+const secretKey = `Impesa123456&`;
 
 const password = `fd505804-3302-439b-8780-a4ab8f6083f8`;
 const password = `fd505804-3302-439b-8780-a4ab8f6083f8`;
