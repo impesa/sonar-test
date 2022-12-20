2deb5f35fd6cd460fb8ff03999ec432ba75ffab0
dfonsecam <dfonseca@impesa.net>
2022-12-20 13:32:53 -0600

    sonar testing

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ async function bootstrap() {
   };
 
   console.log(connection);
+  console.log(connection);
+  console.log(connection);
 
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
