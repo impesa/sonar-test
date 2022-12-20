72b2013df0bda71db7aa4e0cea846e31d0a427ae
dfonsecam <dfonseca@impesa.net>
2022-12-20 14:54:29 -0600

    sonar testing

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const secretKey = `Impesa123456&`;
+console.log(secretKey);
+
+// const password = `some-secret-password`;
+// const password = `fd505804-3302-439b-8780-a4ab8f6083f8`;
+
+const connection = {
+  host: 'localhost',
+  user: 'admin',
+  database: 'project',
+  password: 'mypassword', // sensitive
+  multipleStatements: true,
+};
+
+console.log(connection);
+
 async function bootstrap() {
-  const connection = {
-    host: 'localhost',
-    user: 'admin',
-    database: 'project',
-
-    password: 'thisismysecret',
-
-    multipleStatements: true,
-  };
-
-  console.log(connection);
-  console.log(connection);
-  console.log(connection);
-  console.log(connection);
-  console.log(connection);
-  console.log(connection);
-  console.log(connection);
-  console.log(connection);
-  console.log(connection);
-  console.log(connection);
-
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
