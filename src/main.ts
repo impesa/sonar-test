6ecb5a07d9d8fd2d12b71030aaf0d9f913d834c2
dfonsecam <dfonseca@impesa.net>
2022-12-20 12:36:32 -0600

    sonar testing

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const password = `some-secret-password`;
+  console.log(password);
+
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
