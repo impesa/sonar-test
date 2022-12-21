d2c1eb61bc838274259d879aceb9a404643e12a2
dfonsecam <dfonseca@impesa.net>
2022-12-21 11:27:14 -0600

    test more coverage

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -18,5 +18,8 @@ describe('AppController', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
+    it('should return "Bye World!"', () => {
+      expect(appController.getBye()).toBe('Bye World!');
+    });
   });
 });
